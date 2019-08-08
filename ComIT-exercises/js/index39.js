34c2742f13104a6652fd02d0bc384a2697165763
Kushagra <kusht07@gmail.com>
2019-08-08 18:38:40 -0500

    till String slice 45ex

--- ComIT-exercises/js/index39.js
+++ ComIT-exercises/js/index39.js
@@ -3,5 +3,5 @@ let lastName = 'Trivedi';
 
 console.log(`My name is ${firstName} and it is ${firstName.length} characters long`);
 console.log(`My name is ${lastName} and it is ${lastName.length} characters long`);
-console.log(`The difference between firstname and lastname is ${firstName-lastName}`);
+console.log(`The difference between firstname and lastname is ${firstName.length - lastName.length}`);
 console.log(`My first name is longer than my last name:  ${firstName.length > lastName.length}`);
--- ComIT-exercises/js/index44.js
+++ ComIT-exercises/js/index44.js
@@ -1,10 +1,10 @@
 let text = 'HELLO';
-counter = 1;
-let revert = text.charAt(text.length-counter);
-revert = revert.concat(text.charAt(text.length-(++counter)));
-revert = revert.concat(text.charAt(text.length-(++counter)));
-revert = revert.concat(text.charAt(text.length-(++counter)));
-revert = revert.concat(text.charAt(text.length-(++counter)));
+counter = 0;
+let revert = text.charAt(text.length-(++counter));
+revert += text.charAt(text.length-(++counter));
+revert += text.charAt(text.length-(++counter));
+revert += text.charAt(text.length-(++counter));
+revert += text.charAt(text.length-(++counter));
 
 result = revert.toLowerCase();
-console.log("The final result is: ", result);
+console.log("The final result is:", result);
